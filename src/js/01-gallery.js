e26298911abe6082822870f9ea72a8192a84b6cc
ApolinarySzumski <apolinary@poczta.onet.pl>
2023-04-05 12:44:10 +0200

    finished 1st task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const addtoGallery = galleryItems
+  .map(
+    galleryItem =>
+      `<li><a class="gallery__item" href="${galleryItem.original}">
+    <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+</a></li>
+    `
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('afterbegin', addtoGallery);
+
+const lightGallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+lightGallery.on('show.simplelightbox', function () {});
